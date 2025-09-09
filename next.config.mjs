a18bb8acd6b4a87b7f8fb88e12ef7893f73c400e
whatisyourdreambro <selfdeveloper2020@gmail.com>
2025-09-10 00:38:29 +0900

    fix: 최종 빌드 설정 적용

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,14 @@ import nextMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure MDX to be treated as a page
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 
-  // --- [여기를 추가해주세요] ---
+  // --- [이 부분을 다시 추가해주세요] ---
   typescript: {
     // 빌드 시 타입스크립트 오류가 있어도 무시하고 강제로 빌드합니다.
     ignoreBuildErrors: true,
   },
-  // --------------------------
+  // ------------------------------------
 };
 
 const withMDX = nextMDX();
