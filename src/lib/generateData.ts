1a9698f1f3b2c64f1c88182d9b6d88cd46fa3cfd
whatisyourdreambro <selfdeveloper2020@gmail.com>
2025-09-14 01:47:45 +0900

    50,000

--- src/lib/generateData.ts
+++ src/lib/generateData.ts
@@ -16,7 +16,7 @@ export type SalaryData = {
 export function generateAnnualSalaryTableData(): SalaryData[] {
   const data: SalaryData[] = [];
   // 0원부터 1억원까지 10만원 단위로 생성
-  for (let salary = 0; salary <= 100000000; salary += 100000) {
+  for (let salary = 0; salary <= 100000000; salary += 50000) {
     const results = calculateNetSalary(salary);
     data.push({ preTax: salary, ...results });
   }
@@ -31,8 +31,8 @@ export function generateAnnualSalaryTableData(): SalaryData[] {
 // 월급 실수령액 데이터 생성
 export function generateMonthlySalaryTableData(): SalaryData[] {
   const data: SalaryData[] = [];
-  // 0원부터 1억원까지 10만원 단위로 생성
-  for (let monthly = 0; monthly <= 100000000; monthly += 100000) {
+  // 0원부터 1억원까지 5만원 단위로 생성
+  for (let monthly = 0; monthly <= 100000000; monthly += 50000) {
     const results = calculateNetSalary(monthly * 12);
     data.push({ preTax: monthly, ...results });
   }
@@ -42,8 +42,8 @@ export function generateMonthlySalaryTableData(): SalaryData[] {
 // 주급 실수령액 데이터 생성
 export function generateWeeklyPayTableData(): SalaryData[] {
   const data: SalaryData[] = [];
-  // 0원부터 2000만원까지 10만원 단위로 생성
-  for (let weekly = 0; weekly <= 20000000; weekly += 100000) {
+  // 0원부터 2000만원까지 5만원 단위로 생성
+  for (let weekly = 0; weekly <= 20000000; weekly += 50000) {
     const results = calculateNetSalary(weekly * 52);
     data.push({ preTax: weekly, ...results });
   }
@@ -53,8 +53,8 @@ export function generateWeeklyPayTableData(): SalaryData[] {
 // 시급 실수령액 데이터 생성 (주 40시간, 52주 근무 기준)
 export function generateHourlyWageTableData(): SalaryData[] {
   const data: SalaryData[] = [];
-  // 0원부터 2000만원까지 10만원 단위로 생성
-  for (let hourly = 0; hourly <= 20000000; hourly += 100000) {
+  // 0원부터 2000만원까지 5천원 단위로 생성
+  for (let hourly = 0; hourly <= 20000000; hourly += 5000) {
     const results = calculateNetSalary(hourly * 40 * 52);
     data.push({ preTax: hourly, ...results });
   }
